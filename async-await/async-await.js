36877eef06e1bae20921bb111575f344ef9648ab
Jeareth Venegas <venegasjea@gmail.com>
2021-09-11 21:37:30 -0600

    tarea

--- async-await/async-await.js
+++ async-await/async-await.js
@@ -3,14 +3,18 @@
 // const link = 'https://ghibliapi.herokuapp.com'
 
 async function ghibliFetch(){
-    const response = await fetch ('https://ghibliapi.herokuapp.com/people');
-    const characters = await response.json();
-    console.log(characters);
+    try{
+        const response = await fetch ('https://ghibliapi.herokuapp.com/people');
+        const characters = await response.json();
+        console.log(characters);
+    }catch(error){
+        console.log(error);
+    }
 }
 
 ghibliFetch().then(character =>{
     // error en la consola: Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'map')
-    // at asyncAwait.js:14
+    // at asyncAwait.js:19
     // ¿por qué no lee el map?
     const names = character.map(people => people.name).join('\n')
     console.log (names);
